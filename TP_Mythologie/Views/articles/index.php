e5051446d79341dac9c0e7635c4f33be67a3c5d2
Roman K <101398092+Kasroman@users.noreply.github.com>
2022-05-05 09:44:18 +0200

    Update index.php

--- TP_Mythologie/Views/articles/index.php
+++ TP_Mythologie/Views/articles/index.php
@@ -6,7 +6,7 @@
                 <img class="card-img-top mt-3" src="<?= ROOT_PUBLIC . 'assets/img/articles/' . $article->image_article ?>" alt="Illustration pour <?= $article->titre_article ?>">
                 <div class="card-body">
                     <h5 class="card-title"><?= $article->titre_article ?></h2>
-                    <h6 class="card-subtitle text-muted mb-1">par <span class="fw-bold"><?= ucFirst($article->pseudo_user) ?></span> le <span class="fw-bold fst-italic"><?= $article->date_article ?></span></h6>
+                    <h6 class="card-subtitle text-muted mb-1">par <span class="fw-bold"><?= ucFirst($article->pseudo_user) ?></span> le <span class="fw-bold fst-italic"><?= substr($article->date_article, 0, -3) ?></span></h6>
                     <p class="card-text reset-text">
                         <?= substr(strip_tags(htmlspecialchars_decode($article->contenu_article)), 0, 240) ?> ...
                     </p>
